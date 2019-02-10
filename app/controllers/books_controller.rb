7433a610ca5ea77a6be28b97b8653a3db3fe5b35
fukurou37 <swish3578@gmai.com>
2019-02-10 16:18:47 +0900

    書籍crud

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destory]
+  def index
+    @books = Book.all
+  end
+
+  def new
+      @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book,notice: "書籍を登録しました｡"
+    else
+      render:new 
+    end
+  end
+
+  def show
+  end
+
+  def edit  
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to @book, notice: "書籍を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: "書籍を削除しました。"
+  end
+
+  private
+
+
+  def book_params
+    params.require(:book).permit(:title, :price, :publish_date, :description, :new_image)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+end
